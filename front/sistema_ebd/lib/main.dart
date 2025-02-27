91a51d246c6bbd7313c7c5d89c8143d87590d050
ThalysonE <Thalysonelione8@gmail.com>
2025-02-26 22:16:56 -0300

    tela de login finalizada

--- front/sistema_ebd/lib/main.dart
+++ front/sistema_ebd/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:sistema_ebd/pages/telaInicial.dart';
 import 'package:sistema_ebd/pages/usuario_form.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -30,6 +31,13 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme:ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.light(
+          primary:  Color(0xFF034279),
+        ),
+        textTheme: GoogleFonts.interTextTheme()
+      ),
       home: Scaffold(
         body: logovisivel? Telainicial(): UsuarioForm()
       ),
--- front/sistema_ebd/lib/pages/usuario_form.dart
+++ front/sistema_ebd/lib/pages/usuario_form.dart
@@ -12,87 +12,179 @@ class UsuarioForm extends StatefulWidget {
 
 class _UsuarioFormState extends State<UsuarioForm> {
   final keyform = GlobalKey<FormState>();
+  bool ativadoLembrar = false;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Login',
-            style: TextStyle(
-              fontSize: 35,
-              fontWeight: FontWeight.bold
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Login',
+              style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                fontSize: 35,
+                fontWeight: FontWeight.bold
+              ),
             ),
-          ),
-          Text(
-            'Bem vindo de volta!',
-            style: TextStyle(
-              color: Color(0xFF4B5768),
-              fontSize: 18,
-              fontWeight: FontWeight.w400
+            SizedBox(height: 8,),
+            Text(
+              'Bem vindo de volta!',
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                color: Color(0xFF4B5768),
+                fontSize: 18,
+                fontWeight: FontWeight.w400
+              )
             ),
-          ),
-          SizedBox(height: 30,),
-          Form(
-            key: keyform,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Usuario',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-                SizedBox(height: 10,),
-                TextFormField(
-                  decoration: InputDecoration(
-                    label: Text(
-                      'Erica',
-                      style: TextStyle(
-                        color: Color(0xFFAFADBE),
-                        fontWeight: FontWeight.w500
-                      ),
+            SizedBox(height: 40,),
+            Form(
+              key: keyform,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Usuario',
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold
                     ),
-                    border: OutlineInputBorder(
+                  ),
+                  SizedBox(height: 10,),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1.5,
+                          color: Color(0xFFD0D5DD)
+                        ),
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1.5,
+                          color: Theme.of(context).buttonTheme.colorScheme!.primary
+                        ),
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                      label: Text(
+                        'Erica',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          color: Color(0xFFAFADBE),
+                          fontWeight: FontWeight.w700
+                        ),
+                      ),
+                      floatingLabelBehavior: FloatingLabelBehavior.never
+                    ),
+                    textInputAction: TextInputAction.next,
+                    keyboardType: TextInputType.text,
+                  ),
+                  SizedBox(height: 35,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Senha',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      Text(
+                        'Esqueceu a senha?',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF538CC1)
+                        ),
+                      ),
                       
-                    ),
-                    floatingLabelBehavior: FloatingLabelBehavior.never
+                    ],
                   ),
-                  textInputAction: TextInputAction.next,
-                  keyboardType: TextInputType.text,
-                ),
-                SizedBox(height: 10,),
-                Text(
-                  'Senha',
-                  style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold
+                  SizedBox(height: 10,),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1.5,
+                          color: Color(0xFFD0D5DD)
+                        ),
+                        borderRadius: BorderRadius.circular(10)
                       ),
-                ),
-                SizedBox(height: 10,),
-                TextFormField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    label: Text(
-                      '. . . . . ',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Color(0xFFAFADBE),
-                        fontWeight: FontWeight.bold
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          width: 1.5,
+                          color: Theme.of(context).buttonTheme.colorScheme!.primary
+                        ),
+                        borderRadius: BorderRadius.circular(10)
                       ),
+                      label: Text(
+                        '. . . . . ',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          fontSize: 20,
+                          color: Color(0xFFAFADBE),
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      floatingLabelBehavior: FloatingLabelBehavior.never
                     ),
-                    floatingLabelBehavior: FloatingLabelBehavior.never
+                    keyboardType: TextInputType.visiblePassword,
                   ),
-                  keyboardType: TextInputType.visiblePassword,
-                )
-              ],
-            )
-          ),
-        ],
+                  SizedBox(height: 15,),
+                  Row(
+                    children: [
+                      Switch(
+                        value: ativadoLembrar,
+                        activeColor: Colors.white,
+                        activeTrackColor: Color(0xFF1565C0),
+                        inactiveThumbColor: Color(0xFF1565C0),
+                        onChanged: (value){
+                          setState(() {
+                            ativadoLembrar = value;
+                          });
+                        },
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        'Lembrar de mim',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          fontSize: 12,
+                          color: Color(0xFF6A6C71),
+                          fontWeight: FontWeight.w800
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 42,),
+                  Center(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(vertical: 13, horizontal: 130),
+                        backgroundColor: Color(0xFF1565C0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)
+                        )
+                      ),
+                      onPressed: (){
+                      }, 
+                      child: Text(
+                        'Login',
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 15
+                        ),
+                    
+                      )
+                    ),
+                  )
+                ],
+              )
+            ),
+          ],
+        ),
       ),
     );
   }
