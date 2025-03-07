d03a030203675c82c0eb969b0d8c865885d43916
ThalysonE <Thalysonelione8@gmail.com>
2025-03-07 13:06:36 -0300

    alterando o main

--- front/sistema_ebd/lib/main.dart
+++ front/sistema_ebd/lib/main.dart
@@ -6,12 +6,9 @@ import 'package:sistema_ebd/pages/telaInicial.dart';
 import 'package:sistema_ebd/pages/usuario_form.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:sistema_ebd/utils/rotas.dart';
+
 void main(List<String> args) {
-  runApp(
-    ProviderScope(
-      child: MyApp()
-    )
-  );
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -26,12 +23,12 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_){
-    Timer(const Duration(seconds: 5), (){
-      setState(() {
-        logovisivel = false;
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Timer(const Duration(seconds: 5), () {
+        setState(() {
+          logovisivel = false;
+        });
       });
-    });
     });
   }
 
@@ -39,19 +36,16 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme:ThemeData(
+      theme: ThemeData(
         useMaterial3: true,
-        colorScheme: ColorScheme.light(
-          primary:  Color(0xFF034279),
+        colorScheme: ColorScheme.light(primary: Color(0xFF034279)),
+        textTheme: GoogleFonts.interTextTheme(),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 13, 95, 167),
         ),
-        textTheme: GoogleFonts.interTextTheme()
       ),
-      routes: {
-        AppRotas.login: (context) => UsuarioForm()
-      },
-      home: Scaffold(
-        body: logovisivel? Telainicial(): UsuarioForm()
-      ),
+      routes: {AppRotas.login: (context) => UsuarioForm()},
+      home: Scaffold(body: logovisivel ? Telainicial() : UsuarioForm()),
     );
   }
 }
