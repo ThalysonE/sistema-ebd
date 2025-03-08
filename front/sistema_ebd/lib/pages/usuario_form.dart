ced7b26b2e9f331854c8de542cd7f44f90c7bc6b
ThalysonE <Thalysonelione8@gmail.com>
2025-03-08 18:25:49 -0300

    adicionando loading no botao

--- front/sistema_ebd/lib/pages/usuario_form.dart
+++ front/sistema_ebd/lib/pages/usuario_form.dart
@@ -5,7 +5,6 @@ import 'package:sistema_ebd/Data/providers/usuario_provider.dart';
 import 'package:sistema_ebd/Data/repositories/login_repositories.dart';
 import 'package:sistema_ebd/pages/tela_principal.dart';
 
-
 class UsuarioForm extends ConsumerStatefulWidget {
   const UsuarioForm({super.key});
 
@@ -15,48 +14,56 @@ class UsuarioForm extends ConsumerStatefulWidget {
 
 class _UsuarioFormState extends ConsumerState<UsuarioForm> {
   final keyform = GlobalKey<FormState>();
-  
+
   bool ativadoLembrar = false;
   bool esconderSenha = true;
   bool isLoading = false;
-  String _login ='';
-  String _senha ='';
-  void AutenticarUsuario(WidgetRef refUser) async{
-      keyform.currentState!.save();
-      final usuarioProvider = refUser.read(usuarioLogado.notifier);
-      var statusCode = await usuarioProvider.login(_login, _senha);
-      if(statusCode == 200){
-        Navigator.push(context, MaterialPageRoute(builder: (context)=>TelaPrincipal()));
-      } else if(statusCode == 401){
-        MostrarErro('Nome de usuário ou senha inválidos.');
-      } else if(statusCode == null){
-        MostrarErro('Erro ao tentar realizar o login, tente novamente mais tarde.');
-      }
-    
+  String _login = '';
+  String _senha = '';
+
+  void AutenticarUsuario(WidgetRef refUser) async {
+    print('Chamando');
+    setState(() {
+      isLoading = true;
+    });
+    keyform.currentState!.save();
+    final usuarioProvider = refUser.read(usuarioLogado.notifier);
+    var statusCode = await usuarioProvider.login(_login, _senha);
+    if (statusCode == 200) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => TelaPrincipal()),
+      );
+    } else if (statusCode == 401) {
+      MostrarErro('Nome de usuário ou senha inválidos.');
+    } else if (statusCode == null) {
+      MostrarErro(
+        'Erro ao tentar realizar o login, tente novamente mais tarde.',
+      );
+    }
+    setState(() {
+      isLoading = false;
+    });
   }
-   MostrarErro(String msg){
+
+  MostrarErro(String msg) {
     return ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         backgroundColor: Colors.red,
         duration: Duration(seconds: 2),
         content: Center(
-          child: Text(
-            msg,
-            style: TextStyle(
-              color: Colors.white
-            ),
-          ),
-        )
-      )
+          child: Text(msg, style: TextStyle(color: Colors.white)),
+        ),
+      ),
     );
   }
+
   @override
   void initState() {
     super.initState();
   }
   @override
   Widget build(BuildContext context) {
-    
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 100, horizontal: 30),
@@ -67,19 +74,19 @@ class _UsuarioFormState extends ConsumerState<UsuarioForm> {
               'Login',
               style: Theme.of(context).textTheme.headlineLarge?.copyWith(
                 fontSize: 35,
-                fontWeight: FontWeight.bold
+                fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 8,),
+            SizedBox(height: 8),
             Text(
               'Bem vindo de volta!',
               style: Theme.of(context).textTheme.bodyMedium?.copyWith(
                 color: Color(0xFF4B5768),
                 fontSize: 18,
-                fontWeight: FontWeight.w400
-              )
+                fontWeight: FontWeight.w400,
+              ),
             ),
-            SizedBox(height: 40,),
+            SizedBox(height: 40),
             Form(
               key: keyform,
               child: Column(
@@ -89,64 +96,67 @@ class _UsuarioFormState extends ConsumerState<UsuarioForm> {
                     'Usuário',
                     style: Theme.of(context).textTheme.bodyMedium?.copyWith(
                       fontSize: 14,
-                      fontWeight: FontWeight.bold
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  SizedBox(height: 10,),
+                  SizedBox(height: 10),
                   TextFormField(
                     decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
                         borderSide: BorderSide(
                           width: 1.5,
-                          color: Color(0xFFD0D5DD)
+                          color: Color(0xFFD0D5DD),
                         ),
-                        borderRadius: BorderRadius.circular(10)
+                        borderRadius: BorderRadius.circular(10),
                       ),
                       focusedBorder: OutlineInputBorder(
                         borderSide: BorderSide(
                           width: 1.5,
-                          color: Theme.of(context).buttonTheme.colorScheme!.primary
+                          color:
+                              Theme.of(
+                                context,
+                              ).buttonTheme.colorScheme!.primary,
                         ),
-                        borderRadius: BorderRadius.circular(10)
+                        borderRadius: BorderRadius.circular(10),
                       ),
                       errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 1.5,
-                          color: Colors.red
-                        ),
-                        borderRadius: BorderRadius.circular(10)
+                        borderSide: BorderSide(width: 1.5, color: Colors.red),
+                        borderRadius: BorderRadius.circular(10),
                       ),
                       focusedErrorBorder: OutlineInputBorder(
                         borderSide: BorderSide(
                           width: 1.5,
-                          color: Theme.of(context).buttonTheme.colorScheme!.primary
+                          color:
+                              Theme.of(
+                                context,
+                              ).buttonTheme.colorScheme!.primary,
                         ),
-                        borderRadius: BorderRadius.circular(10)
+                        borderRadius: BorderRadius.circular(10),
                       ),
                       label: Text(
                         'Erica',
                         style: Theme.of(context).textTheme.bodyMedium?.copyWith(
                           color: Color(0xFFAFADBE),
-                          fontWeight: FontWeight.w700
+                          fontWeight: FontWeight.w700,
                         ),
                       ),
-                      floatingLabelBehavior: FloatingLabelBehavior.never
+                      floatingLabelBehavior: FloatingLabelBehavior.never,
                     ),
                     textInputAction: TextInputAction.next,
                     keyboardType: TextInputType.text,
-                    validator: (value){
-                      if(value == null || value.isEmpty){
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
                         return 'Por favor, insira um nome de usuário.';
-                      }else if(value.length<3){
+                      } else if (value.length < 3) {
                         return 'Nome de usuário deve ter mais de 3 caracteres';
                       }
                       return null;
                     },
-                    onSaved: (value){
+                    onSaved: (value) {
                       _login = value!;
                     },
                   ),
-                  SizedBox(height: 35,),
+                  SizedBox(height: 35),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -154,7 +164,7 @@ class _UsuarioFormState extends ConsumerState<UsuarioForm> {
                         'Senha',
                         style: Theme.of(context).textTheme.bodyMedium?.copyWith(
                           fontSize: 14,
-                          fontWeight: FontWeight.bold
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                       Text(
@@ -162,76 +172,82 @@ class _UsuarioFormState extends ConsumerState<UsuarioForm> {
                         style: Theme.of(context).textTheme.bodyMedium?.copyWith(
                           fontSize: 12,
                           fontWeight: FontWeight.bold,
-                          color: Color(0xFF538CC1)
+                          color: Color(0xFF538CC1),
                         ),
                       ),
-                      
                     ],
                   ),
-                  SizedBox(height: 10,),
+                  SizedBox(height: 10),
                   TextFormField(
                     decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
                         borderSide: BorderSide(
                           width: 1.5,
-                          color: Color(0xFFD0D5DD)
+                          color: Color(0xFFD0D5DD),
                         ),
-                        borderRadius: BorderRadius.circular(10)
+                        borderRadius: BorderRadius.circular(10),
                       ),
                       focusedBorder: OutlineInputBorder(
                         borderSide: BorderSide(
                           width: 1.5,
-                          color: Theme.of(context).buttonTheme.colorScheme!.primary
+                          color:
+                              Theme.of(
+                                context,
+                              ).buttonTheme.colorScheme!.primary,
                         ),
-                        borderRadius: BorderRadius.circular(10)
+                        borderRadius: BorderRadius.circular(10),
                       ),
                       errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 1.5,
-                          color: Colors.red
-                        ),
-                        borderRadius: BorderRadius.circular(10)
+                        borderSide: BorderSide(width: 1.5, color: Colors.red),
+                        borderRadius: BorderRadius.circular(10),
                       ),
                       focusedErrorBorder: OutlineInputBorder(
                         borderSide: BorderSide(
                           width: 1.5,
-                          color: Theme.of(context).buttonTheme.colorScheme!.primary
+                          color:
+                              Theme.of(
+                                context,
+                              ).buttonTheme.colorScheme!.primary,
                         ),
                         borderRadius: BorderRadius.circular(10),
                       ),
                       suffixIcon: IconButton(
-                        onPressed: (){
+                        onPressed: () {
                           setState(() {
                             esconderSenha = !esconderSenha;
                           });
-                        }, 
-                        icon: Icon(esconderSenha?Icons.remove_red_eye_outlined:Icons.remove_red_eye)
+                        },
+                        icon: Icon(
+                          esconderSenha
+                              ? Icons.remove_red_eye_outlined
+                              : Icons.remove_red_eye,
+                        ),
                       ),
-                      
+
                       label: Text(
                         '. . . . . ',
                         style: Theme.of(context).textTheme.bodyMedium?.copyWith(
                           color: Color(0xFFAFADBE),
-                          fontWeight: FontWeight.bold
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      floatingLabelBehavior: FloatingLabelBehavior.never
+                      floatingLabelBehavior: FloatingLabelBehavior.never,
                     ),
-                    keyboardType:TextInputType.visiblePassword,
+                    keyboardType: TextInputType.visiblePassword,
                     obscureText: esconderSenha,
-                    validator: (value){
-                      if(value == null || value.isEmpty){
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
                         return 'Por favor, insira uma senha válida.';
-                      }else if(value.length < 8){
+                      } else if (value.length < 8) {
                         return 'Insira uma senha maior que 8 digitos';
-                      } 
+                      }
                       return null;
                     },
-                    onSaved: (value){
+                    onSaved: (value) {
                       _senha = value!;
                     },
                   ),
-                  SizedBox(height: 15,),
+                  SizedBox(height: 15),
                   Row(
                     children: [
                       Switch(
@@ -239,53 +255,68 @@ class _UsuarioFormState extends ConsumerState<UsuarioForm> {
                         activeColor: Colors.white,
                         activeTrackColor: Color(0xFF1565C0),
                         inactiveThumbColor: Color(0xFF1565C0),
-                        onChanged: (value){
+                        onChanged: (value) {
                           setState(() {
                             ativadoLembrar = value;
                           });
                         },
                       ),
-                      SizedBox(
-                        width: 5,
-                      ),
+                      SizedBox(width: 5),
                       Text(
                         'Lembrar de mim',
                         style: Theme.of(context).textTheme.bodyMedium?.copyWith(
                           fontSize: 12,
                           color: Color(0xFF6A6C71),
-                          fontWeight: FontWeight.w800
+                          fontWeight: FontWeight.w800,
                         ),
                       ),
                     ],
                   ),
-                  SizedBox(height: 42,),
+                  SizedBox(height: 42),
                   Center(
                     child: ElevatedButton(
                       style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.symmetric(vertical: 13, horizontal: 130),
+                        elevation: 2,
+                        padding: EdgeInsets.symmetric(
+                          vertical: 13,
+                          horizontal: 130,
+                        ),
                         backgroundColor: Color(0xFF1565C0),
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15)
-                        )
-                      ),
-                      onPressed: (){
-                        if(keyform.currentState!.validate()){
-                          AutenticarUsuario(ref);
-                        }
-                      }, 
-                      child: Text(
-                        'Login',
-                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                          fontSize: 15
+                          borderRadius: BorderRadius.circular(15),
                         ),
-                    
-                      )
+                      ),
+                      onPressed:
+                          isLoading
+                              ? null
+                              : () {
+                                if (keyform.currentState!.validate()) {
+                                  AutenticarUsuario(ref);
+                                }
+                              },
+                      child:
+                          isLoading
+                              ? SizedBox(
+                                height: 20,
+                                width: 20,
+                                child: CircularProgressIndicator(
+                                  color: Color(0xFF1565C0),
+                                ),
+                              )
+                              : Text(
+                                'Login',
+                                style: Theme.of(
+                                  context,
+                                ).textTheme.bodyMedium?.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                ),
+                              ),
                     ),
-                  )
+                  ),
                 ],
-              )
+              ),
             ),
           ],
         ),
