3080d9649452c1c7c3237d4ea77d4d008e54c9b9
ThalysonE <Thalysonelione8@gmail.com>
2025-03-12 18:20:22 -0300

    alterando o formato da classe membro

--- front/sistema_ebd/lib/models/membro.dart
+++ front/sistema_ebd/lib/models/membro.dart
@@ -3,13 +3,12 @@ class Membro {
   String nome;
   String dataDeNascimento;
   String sexo;
-  int idade;
+
 
   Membro({
     required this.nome,
     required this.dataDeNascimento,
     required this.sexo,
-    required this.idade,
   });
 
   factory Membro.fromMap(Map<String, dynamic> map) {
@@ -17,7 +16,6 @@ class Membro {
       nome: map["name"],
       dataDeNascimento: map["birthDate"],
       sexo: map["sex"],
-      idade: map["age"],
     );
   }
 }
