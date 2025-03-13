f0b8fc84182b4130926d42955ac58e8c7881cb0a
ThalysonE <Thalysonelione8@gmail.com>
2025-03-13 10:44:52 -0300

    adicionando o link da api como global

--- front/sistema_ebd/lib/Data/variaveisGlobais/variaveis_globais.dart
+++ front/sistema_ebd/lib/Data/variaveisGlobais/variaveis_globais.dart
@@ -1,3 +1,7 @@
 // Utilizado na tela de membros
 int totalMembros = 0; //guarda a quantidade total de membros cadastrados
 int paginaAtual = 1; // guarda até qual pagina foi a requisição de membros
+
+
+//link da api
+String apiUrl = 'http://192.168.0.75:3333';
