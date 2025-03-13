6a1daf91ed0ce6a5dd64eca74c81c32fb2158f0c
ThalysonE <Thalysonelione8@gmail.com>
2025-03-13 10:43:31 -0300

    mudando de lugar a widget da appbar

---
+++ front/sistema_ebd/lib/Widgets/appbar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+AppBar CriarAppBar(context, String title){
+  return AppBar(
+    backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+          centerTitle: true,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.chevron_left, size: 30),
+            color: Colors.white,
+          ),
+          title: Text(
+            title,
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+              fontSize: 16.5,
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+          ),
+  );
+}
