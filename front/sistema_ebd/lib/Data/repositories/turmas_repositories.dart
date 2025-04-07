d11b04a6de6724a06aede88821bed6a0e955e723
ThalysonE <Thalysonelione8@gmail.com>
2025-04-07 18:04:10 -0300

    turmas post e get

---
+++ front/sistema_ebd/lib/Data/repositories/turmas_repositories.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+
+import 'package:sistema_ebd/Data/http/http_client.dart';
+import 'package:sistema_ebd/Data/variaveisGlobais/variaveis_globais.dart';
+import 'package:sistema_ebd/models/turma.dart';
+
+abstract class IturmasRepository {
+  Future<List<Turma>?> getTurmas(int numeroPagina, String token);
+  Future<int?> postTurmas({
+    required String name,
+    required String token,
+    int? minAge = null,
+    int? maxAge = null,
+  });
+}
+
+class TurmasRepositories extends IturmasRepository {
+  final IHttpClient client;
+  TurmasRepositories(this.client);
+
+  Future<List<Turma>?> getTurmas(int numeroPagina, String token) async {
+    final url = Uri.parse(
+      apiUrl + '/room',
+    ).replace(queryParameters: {'page': numeroPagina.toString()});
+    List<Turma> turmas = [];
+    try {
+      final resposta = await client.get(url: url, token: token);
+      if (resposta.statusCode == 200) {
+        final body = jsonDecode(resposta.body);
+        body['rooms'].map((turmaMap) {
+          final turma = Turma.fromMap(turmaMap);
+          turmas.add(turma);
+        }).toList();
+        return turmas;
+      }
+    } catch (e) {
+      print('Erro na requisição de turmas');
+    }
+    return null;
+  }
+
+  Future<int?> postTurmas({
+    required String name,
+    required String token,
+    int? minAge = null,
+    int? maxAge = null,
+  }) async {
+    final url = apiUrl + 'room';
+    final body = {'name': name, 'minAge': minAge, 'maxAge': maxAge};
+    try {
+      final resposta = await client.post(url: url, body: body, token: token);
+      if (resposta.statusCode == 200) {
+        print('Cadastro realizado com sucesso!');
+      } else {
+        print('Erro no cadastro');
+      }
+      return resposta.statusCode;
+    } catch (e) {
+      print('Erro na requisição: ${e.toString()}');
+      return null;
+    }
+  }
+}
---
+++ front/sistema_ebd/lib/models/turma.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+
+class Turma {
+  String name;
+  int? minAge;
+  int? maxAge;
+
+  Turma({required this.name, this.minAge, this.maxAge});
+  
+  factory Turma.fromMap(Map<String, dynamic> map){
+    return Turma(
+      name: map['name'],
+      minAge: map['minAge'],
+      maxAge: map['maxAge']
+    );
+  }
+  
+}
