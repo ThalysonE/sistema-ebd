8c1072e6c08f020add205320f4a9388f530103ae
ThalysonE <Thalysonelione8@gmail.com>
2025-04-20 11:33:12 -0300

    modularizando o cadastro de turma

--- front/sistema_ebd/lib/pages/turmas.dart
+++ front/sistema_ebd/lib/pages/turmas.dart
@@ -4,7 +4,7 @@ import 'package:sistema_ebd/Data/providers/usuario_provider.dart';
 import 'package:sistema_ebd/Data/repositories/turmas_repositories.dart';
 import 'package:sistema_ebd/Data/variaveisGlobais/variaveis_globais.dart';
 import 'package:sistema_ebd/Widgets/appbar.dart';
-import 'package:sistema_ebd/Widgets/listWhell.dart';
+import 'package:sistema_ebd/Widgets/dialog_cadastro_turma.dart';
 import 'package:sistema_ebd/models/turma.dart';
 import 'package:sistema_ebd/pages/forms/trimestre/turmas_professor.dart';
 
@@ -27,15 +27,6 @@ class _TurmasState extends ConsumerState<Turmas> {
   bool novasTurmas = false;
   final requisicaoTurmas = TurmasRepositories();
 
-  GlobalKey<FormState>? _formKey;
-
-  //variaveis para gerenciar a idade escolhida no form 
-  ValueNotifier<int>? idadeMaxEscolhida;
-  ValueNotifier<int>? idadeMinEscolhida;
-
-  TextEditingController? _nomeController;
-  TextEditingController? _idadeMinController;
-  TextEditingController? _idadeMaxController;
   Future<void> fetchTurmas(int numeroPag) async {
     final fetchTurmas = await requisicaoTurmas.getTurmas(
       numeroPag,
@@ -60,17 +51,6 @@ class _TurmasState extends ConsumerState<Turmas> {
     super.initState();
     usuarioLogadoUser = ref.read(usuarioLogado);
     fetchTurmas(numeroPaginaTurmas++);
-    if (widget.temCadastro) {
-      //controllers do form
-      _formKey = GlobalKey<FormState>();
-      _nomeController = TextEditingController();
-      _idadeMaxController = TextEditingController();
-      _idadeMinController = TextEditingController();
-
-      //variaveis da idade
-      idadeMaxEscolhida = ValueNotifier<int>(0);
-      idadeMinEscolhida = ValueNotifier<int>(0);
-    }
     _controllerScroll.addListener(() {
       if (_controllerScroll.position.maxScrollExtent ==
           _controllerScroll.offset) {
@@ -108,319 +88,16 @@ class _TurmasState extends ConsumerState<Turmas> {
     );
   }
 
-  void postTurma() async {
-    if (_formKey!.currentState!.validate()) {
-      final resposta = await requisicaoTurmas.postTurmas(
-        name: _nomeController!.text,
-        token: usuarioLogadoUser.token,
-        minAge:
-            _idadeMinController!.text.isEmpty
-                ? null
-                : int.parse(_idadeMinController!.text),
-        maxAge:
-            _idadeMaxController!.text.isEmpty
-                ? null
-                : int.parse(_idadeMaxController!.text),
-      );
-      if (resposta == 201) {
-        numeroPaginaTurmas = 1;
-        turmas = [];
-        fetchTurmas(numeroPaginaTurmas++);
-        mostrarMsg(1);
-      } else {
-        mostrarMsg(0);
-      }
-      Navigator.pop(context);
-    }
-  }
+  
 
-  selecionarIdade(int idadeMin, int tipo) {
-    return showModalBottomSheet(
-      // isDismissible: false,
-      enableDrag: false,
-      backgroundColor: Color(0xFFececec),
-      context: context,
-      builder: (context) {
-        return Container(
-          height: 390,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Expanded(child: ListaIdade(idadeMin: idadeMin, idadeAtual: tipo==0? idadeMinEscolhida!:idadeMaxEscolhida!,)),
-              Align(
-                alignment: Alignment.center,
-                child: ElevatedButton(
-                  onPressed: () {
-                    if(tipo==0){
-                      _idadeMinController!.text = idadeMinEscolhida!.value.toString();
-                    }else{
-                      _idadeMaxController!.text = idadeMaxEscolhida!.value.toString();
-                    }
-                    Navigator.pop(context);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(
-                      vertical: 13,
-                      horizontal: 100,
-                    ),
-                    backgroundColor: Color(0xFF1565C0),
-                    elevation: 2,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                  ),
-                  child: Text(
-                    'Escolher',
-                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                      color: Colors.white,
-                      fontSize: 17,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 8),
-            ],
-          ),
-        );
-      },
-    );
-  }
+  
 
   //colocar em outra pagina
   cadastro() {
     showDialog(
       context: context,
       builder: (context) {
-        return AlertDialog(
-          title: Text(
-            'Cadastro Turma',
-            style: Theme.of(context).textTheme.titleMedium!.copyWith(
-              fontSize: 17,
-              fontWeight: FontWeight.bold,
-            ),
-            textAlign: TextAlign.center,
-          ),
-          content: SingleChildScrollView(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 12),
-                  Text(
-                    'Nome da turma: ',
-                    style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                      fontSize: 13,
-                      fontWeight: FontWeight.w600,
-                      color: const Color.fromARGB(181, 0, 0, 0),
-                    ),
-                  ),
-                  SizedBox(height: 15),
-                  TextFormField(
-                    controller: _nomeController,
-                    decoration: InputDecoration(
-                      filled: true,
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 1.5,
-                          color: Color(0xFFD0D5DD),
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 1.5,
-                          color:
-                              Theme.of(context).buttonTheme.colorScheme!.primary,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(width: 1.5, color: Colors.red),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      focusedErrorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 1.5,
-                          color:
-                              Theme.of(context).buttonTheme.colorScheme!.primary,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      floatingLabelBehavior: FloatingLabelBehavior.never,
-                      label: Text(
-                        'Nome da Turma',
-                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                          color: Colors.black54,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'O nome deve ter mais de 3 caracteres';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 12),
-                  Text(
-                    'Idade Mínima ',
-                    style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                      fontSize: 13,
-                      fontWeight: FontWeight.w600,
-                      color: const Color.fromARGB(181, 0, 0, 0),
-                    ),
-                  ),
-                  SizedBox(height: 15),
-                  TextFormField(
-                    readOnly: true,
-                    onTap: (){
-                      selecionarIdade(0,0);
-                    },
-                    controller: _idadeMinController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      filled: true,
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 1.5,
-                          color: Color(0xFFD0D5DD),
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 1.5,
-                          color:
-                              Theme.of(context).buttonTheme.colorScheme!.primary,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(width: 1.5, color: Colors.red),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      focusedErrorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 1.5,
-                          color:
-                              Theme.of(context).buttonTheme.colorScheme!.primary,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      floatingLabelBehavior: FloatingLabelBehavior.never,
-                      label: Text(
-                        'Idade minima',
-                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                          color: Colors.black54,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Idade Inválida';
-                      }
-                      return null;
-                    },
-                  ),
-                  SizedBox(height: 12),
-                  Text(
-                    'Idade Maxima ',
-                    style: Theme.of(context).textTheme.labelMedium!.copyWith(
-                      fontSize: 13,
-                      fontWeight: FontWeight.w600,
-                      color: const Color.fromARGB(181, 0, 0, 0),
-                    ),
-                  ),
-                  SizedBox(height: 15),
-                  TextFormField(
-                    readOnly: true,
-                    onTap: (){
-                      selecionarIdade(int.parse(_idadeMinController!.text)+1, 1);
-                    },
-                    controller: _idadeMaxController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      filled: true,
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 1.5,
-                          color: Color(0xFFD0D5DD),
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 1.5,
-                          color:
-                              Theme.of(context).buttonTheme.colorScheme!.primary,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(width: 1.5, color: Colors.red),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      focusedErrorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          width: 1.5,
-                          color:
-                              Theme.of(context).buttonTheme.colorScheme!.primary,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      floatingLabelBehavior: FloatingLabelBehavior.never,
-                      label: Text(
-                        'Idade Máxima',
-                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                          color: Colors.black54,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Idade Inválida';
-                      }
-                      return null;
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
-          actions: [
-            Align(
-              alignment: Alignment.center,
-              child: ElevatedButton.icon(
-                icon: Icon(Icons.add_circle_outline, color: Colors.white),
-                onPressed: () {
-                  postTurma();
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 80),
-                  backgroundColor: Color(0xFF1565C0),
-                  elevation: 2,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-                label: Text(
-                  'Cadastrar',
-                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                    color: Colors.white,
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        );
+        return CadastroDialog(usuarioToken: usuarioLogadoUser.token, recarregarTurmas: recarregarTurmas, mostrarMsg: mostrarMsg);
       },
     );
   }
@@ -440,10 +117,6 @@ class _TurmasState extends ConsumerState<Turmas> {
   @override
   void dispose() {
     _controllerScroll.dispose();
-
-    _nomeController?.dispose();
-    _idadeMaxController?.dispose();
-    _idadeMinController?.dispose();
     super.dispose();
   }
 
